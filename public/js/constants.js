dffc16774f494058041d50a4ad7b7a6394c374b4
Sam Pepose <sam.pepose@oasisdigital.com>
2015-07-18 14:40:09 -0700

    Added a FAQ.

---
+++ public/js/constants.js
@@ -0,0 +1,36 @@
+module.exports = {
+  FAQ : [
+    {
+      "question": "How does registration work?",
+      "answer": "We currently only have preregistration open. Registration will open on ___ and close on ____. Admission is determined by a variety of factors. Due to a limited budget, we cannot have a random lottery."
+    },
+    {
+      "question": "What should I bring?",
+      "answer": "Be sure to bring a photo ID, laptop, phone, chargers, a change of clothes, toiletries, and your best thinking cap! A government-issued ID is required to rent any hardware."
+    },
+    {
+      "question": "How do I get there?",
+      "answer": "Buses or flights, depending on location! We’ll be sending buses to schools with a large number of applicants. Travel reimbursement outside of the bus routes will depend on our budget, TBD."
+    },
+    {
+      "question": "Where will I sleep?",
+      "answer": "We will have a couple designated quiet rooms, so bring a blanket and pillows!"
+    },
+    {
+      "question": "What will I eat?",
+      "answer": "We will provide all meals and snacks from Friday's dinner until Sunday's lunch. We’ll be providing vegetarian and gluten-free options."
+    },
+    {
+      "question": "How do teams work?",
+      "answer": "Teams are limited to 4 people in size. If you're applying with a team, we will review your entire team's applications to determine admission."
+    },
+    {
+      "question": "What if I’ve never been to a hackathon?",
+      "answer": "Even more of a reason for you to come! In fact, we’ll have beginners’ workshops for you to get started, and our mentors can help you out along the way."
+    },
+    {
+      "question": "My question wasn’t addressed here. To whom should I reach out?",
+      "answer": "Shoot us an email at info@vandyhacks.org."
+    },
+  ]
+};
