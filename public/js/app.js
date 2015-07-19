936b58b3fe9f7128f67d4078e23dd80464f334ed
Sam Pepose <sam.pepose@oasisdigital.com>
2015-07-19 14:26:11 -0700

    sticky-scroll navbar, moved some deps to local, added some imgs for later #jqueryhax0rs

--- public/js/app.js
+++ public/js/app.js
@@ -10,15 +10,15 @@ var LOCALHOST = 'localhost';
 var FB_PROD_ID = 508263295995091;
 var FB_DEV_ID = 524904174331003;
 
-Parse.initialize("DfoMH2OG5zwZ2Fsr0cbcuYkT2NFSrq89zBRIah3H", "UQRnGd3jIMfcg3HLg0sTQhqCdIFcHCx1yPeaE3nP");
-Parse.FacebookUtils.init({
-  appId: window.location.host.slice(0, LOCALHOST.length) === LOCALHOST ?
-    FB_DEV_ID :
-    FB_PROD_ID,
-  cookie: true,  // enable cookies to allow Parse to access the session
-  xfbml: true,  // initialize Facebook social plugins on the page
-  version: 'v2.3' // point to the latest Facebook Graph API version
-});
+// Parse.initialize("DfoMH2OG5zwZ2Fsr0cbcuYkT2NFSrq89zBRIah3H", "UQRnGd3jIMfcg3HLg0sTQhqCdIFcHCx1yPeaE3nP");
+// Parse.FacebookUtils.init({
+//   appId: window.location.host.slice(0, LOCALHOST.length) === LOCALHOST ?
+//     FB_DEV_ID :
+//     FB_PROD_ID,
+//   cookie: true,  // enable cookies to allow Parse to access the session
+//   xfbml: true,  // initialize Facebook social plugins on the page
+//   version: 'v2.3' // point to the latest Facebook Graph API version
+// });
 
 React.render(
   <ImageCarousel />,
@@ -28,7 +28,53 @@ React.render(
   <FAQ questions={constants.FAQ} />,
   document.getElementById('faq')
 );
+// React.render(
+//   <PreRegisterBox />,
+//   document.getElementById('react-hook')
+// );
+
+var Bootstrap = require('react-bootstrap');
+var {Navbar, Nav, NavItem} = Bootstrap;
+
+
 React.render(
-  <PreRegisterBox />,
-  document.getElementById('react-hook')
+  <Navbar>
+    <div className="navbar-header">
+      <img className="navbar-logo navbar-brand" src="../img/logo.svg" height={35} />
+    </div>
+    <Nav>
+      <NavItem eventKey={1} href='#about'>About</NavItem>
+      <NavItem eventKey={2} href='#faq'>FAQ</NavItem>
+      <NavItem eventKey={2} href='#spnsrs'>Sponsors</NavItem>
+    </Nav>
+  </Navbar>,
+  document.getElementById('nav')
 );
+
+var onscroll = function() {
+  if ($(window).width() > 768) {
+    if ($(window).scrollTop() > $('.navbar').offset().top) {
+      $(".navbar").addClass('fixed');
+      $(".header-container").css('margin-bottom', $('.navbar').outerHeight(true));
+    } else if ($(window).scrollTop() < $(".header-container").outerHeight()) {
+      $(".header-container").css('margin-bottom', 0);
+      $(".navbar").removeClass('fixed');
+    }
+  }
+};
+
+$(window).on({
+  'scrollstart': onscroll,
+  'scroll': onscroll,
+});
+
+
+var widest = null;
+$("*").each(function() {
+  if (widest == null)
+    widest = $(this);
+  else
+  if ($(this).width() > widest.width())
+    widest = $(this);
+});
+console.log(widest)
