cfc523fc6aa426b5d0df6289869b8390d08d2200
Jeffrey Gao <jeffgaoabq@gmail.com>
2015-07-20 01:57:52 -0700

    button margin changes

--- public/js/Main.react.js
+++ public/js/Main.react.js
@@ -22,7 +22,7 @@ var Main = React.createClass({
           <div className="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-6">
             <h1 className="title gold text-center">What is <span className="gold">VandyHacks</span>?</h1>
             <p>VandyHacks is the premiere university hackathon in Nashville, TN, featuring <span className="gold">250+ students</span>. Join us on <span className="gold">October 2-4, 2015</span> at <span className="gold">Vanderbilt University</span>.</p>
-            <div className="text-center">
+            <div className="text-center action-buttons">
               <a className="button" href="#apply">Apply</a>
               <a className="button" href="mailto:info@vandyhacks.org">Sponsor</a>
               <a className="button" href="http://goo.gl/forms/or9W0jceFv" target="_mentors">Mentor</a>
