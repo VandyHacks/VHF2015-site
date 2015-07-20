2c31d654c587b127b646dc037f367aee41db9002
Sam Pepose <sam.pepose@oasisdigital.com>
2015-07-20 00:05:15 -0700

    tried to cleanup css a little

--- public/js/Main.react.js
+++ public/js/Main.react.js
@@ -22,7 +22,7 @@ var Main = React.createClass({
                 <ImageCarousel />
               </div>
               <div className="col-xs-12 col-sm-8">
-                <h1 className="header main-header text-center">What is <span className="gold">VandyHacks</span>?</h1>
+                <h1 className="title main-header text-center">What is <span className="gold">VandyHacks</span>?</h1>
                 <p className="text-center">VandyHacks is the premiere university hackathon in Nashville, TN, featuring <span className="gold">250+ students</span>. Join us on <span className="gold">October 2-4, 2015</span> at <span className="gold">Vanderbilt University</span>.</p>
                 <div className="text-center">
                   <a className="button" href="#apply">Apply</a>
@@ -36,9 +36,9 @@ var Main = React.createClass({
       </Element>
 
       <Element name="faq" className="element">
-        <div className="row faq-section">
+        <div className="row faq">
           <div className="col-xs-10 col-xs-offset-1">
-            <h1 className="header gold text-center">Frequently Asked Questions</h1>
+            <h1 className="title gold text-center">Frequently Asked Questions</h1>
             <FAQ questions={constants.FAQ} />
           </div>
         </div>
@@ -47,7 +47,7 @@ var Main = React.createClass({
       <div className="row">
         <div className="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-4 text-center">
           <a name="apply"></a>
-          <h1 className="header gold">Pre-register</h1>
+          <h1 className="title gold">Pre-register</h1>
           <PreRegisterBox />
         </div>
       </div>
@@ -55,7 +55,7 @@ var Main = React.createClass({
       <Element name="spnsrs" className="element">
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1 text-center">
-            <h1 className="header gold">Sponsors</h1>
+            <h1 className="title gold">Sponsors</h1>
             <a href="http://www.digitalreasoning.com/" target="_DR">
               <img src="img/logos/DR.png" className="spnsr-logo" />
             </a>
@@ -66,7 +66,7 @@ var Main = React.createClass({
 
       <div className="row">
         <div className="col-xs-10 col-xs-offset-1 text-center">
-          <h1 className="header gold">Partners</h1>
+          <h1 className="title gold">Partners</h1>
           <img src="img/logos/mlh.svg" className="spnsr-logo" />
         </div>
       </div>
